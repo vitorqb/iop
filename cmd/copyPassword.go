d33cc1cce004dc78f118d01c3affbe79ac087f61
Vitor Barbosa <vitorqb@gmail.com>
2022-05-07 15:47:11 +0200

    NEW Prompts user for item title if null

--- cmd/copyPassword.go
+++ cmd/copyPassword.go
@@ -7,6 +7,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/vitorqb/iop/package/opClient"
+	"github.com/vitorqb/iop/package/system"
 )
 
 var copyPasswordCmd = &cobra.Command{
@@ -16,6 +17,18 @@ var copyPasswordCmd = &cobra.Command{
 		itemRef, _ := cmd.Flags().GetString("ref")
 		client := opClient.New()
 		client.EnsureLoggedIn()
+
+		// TODO -> If we init a system here, why not pass it to opClient?
+		if itemRef == "" {
+			system := system.New()
+			titles := client.ListItemTitles()
+			selectedTitle, err := system.AskUserToSelectString(titles)
+			if err != nil {
+				system.Crash("Something went wrong asking user to select title", err)
+			}
+			itemRef = selectedTitle
+		}
+
 		password := client.GetPassword(itemRef)
 		clipboard.WriteAll(password)
 		log.Println("Copied to clipboard!")
@@ -24,7 +37,6 @@ var copyPasswordCmd = &cobra.Command{
 
 func init() {
 	copyPasswordCmd.Flags().StringP("ref", "n", "", "Reference (name,id,link) of the item to copy.")
-	copyPasswordCmd.MarkFlagRequired("ref")
 
 	rootCmd.AddCommand(copyPasswordCmd)
 }
