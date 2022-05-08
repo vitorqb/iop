038d9dece38999932d75c4f3c882836dc207f33d
Vitor Barbosa <vitorqb@gmail.com>
2022-05-08 09:28:52 +0200

    NEW Adds NewTempFile to tempScript package

--- package/tempScript/main.go
+++ package/tempScript/main.go
@@ -1,3 +1,4 @@
+// !!!! TODO RENAME PACKAGE TO TEMPFILE
 package tempScript
 
 import (
@@ -5,32 +6,48 @@ import (
 	"os"
 )
 
-type tempScript struct {
-	body string
-}
-
-func New(body string) tempScript {
-	return tempScript{body: body}
-}
-
-func (t tempScript) Run(f func(path string)) error {
+// Allows user to run a function with a temporary file path
+type tempFile struct {}
+func (tempFile) Run(f func(file *os.File) error) error {
 	file, err := ioutil.TempFile("", "test-file")
 	if err != nil {
 		return err
 	}
-	_, err = file.WriteString(t.body)
+	err = f(file)
 	if err != nil {
 		return err
 	}
-	err = file.Close()
-	if err != nil {
+	os.Remove(file.Name())
+	return nil
+}
+func NewTempFile() tempFile {
+	return tempFile{}
+}
+
+// Allows user to save a script to a temporary file, and run a script with it's path
+type tempScript struct {
+	body string
+}
+
+func (t tempScript) Run(f func(path string)) error {
+	return NewTempFile().Run(func(file *os.File) error {
+		_, err := file.WriteString(t.body)
+		if err != nil {
+			return err
+		}
+		err = file.Close()
+		if err != nil {
+			return err
+		}
+		err = os.Chmod(file.Name(), 0777)
+		if err != nil {
+			return err
+		}
+		f(file.Name())
 		return err
-	}
-	err = os.Chmod(file.Name(), 0777)
-	if err != nil {
-		return err
-	}
-	f(file.Name())
-	err = os.Remove(file.Name())
-	return err
+	})
+}
+
+func NewTempScript(body string) tempScript {
+	return tempScript{body: body}
 }
