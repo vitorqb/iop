2fb4c937d6f36505d9d65013b34b2ba56e675727
Vitor Barbosa <vitorqb@gmail.com>
2022-06-17 11:14:20 +0200

    Adds release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-USAGE="$0"' [-h]
-Builds the binary.
+USAGE="$0"' [-h] VERSION
+Creates a gh release.
 
 -h)
   Display this help msg
 '
+
+set -e
 
 # Import utils.sh
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
@@ -16,12 +18,15 @@ OPTS="$(getopt --options $SHORT --name "$0" -- "$@")"
 ! [ "$?" = 0 ] && echo "$USAGE" 1>&2 && exit 1
 while [[ "$#" -gt 0 ]]
 do
+    echo "$1"
     case "$1" in
         -h)
             echo "$USAGE" >&2
             exit 0
             ;;
         --)
+            shift
+            VERSION="$1"
             shift
             ;;
         *)
@@ -30,7 +35,18 @@ do
     esac
 done
 
+# Sanity check
+if [[ ! $VERSION =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]
+then
+    err "Version should be of format x.y.z, but received $VERSION"
+fi
+
 # Script
-run mkdir -p ./dist
-run go build -o ./dist/iop
-run bash -c '( cd dist && tar -vzcf iop.tar.gz iop)'
+GH_TOKEN_FILE="${SCRIPT_DIR}/../.gh_token"
+if [ -r $GH_TOKEN_FILE ]
+then
+    msg "Reading GH_TOKEN from $GH_TOKEN_FILE"
+    export GH_TOKEN="$(cat $GH_TOKEN_FILE | tr -d '\n' )"
+fi
+run ${SCRIPT_DIR}/build.sh
+run gh release create "$VERSION" --generate-notes ./dist/iop.tar.gz
